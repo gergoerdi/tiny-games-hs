55c69c6fd60be42aca8c0884541fba6116a2add6
Gergo ERDI <gergo@erdi.hu>
2023-02-13 21:59:57 +0800

    shrink

--- hackage/vaders/Imports.hs
+++ hackage/vaders/Imports.hs
@@ -1,15 +1,15 @@
 module Imports
   ( module Graphics.Gloss.Interface.Pure.Game
-  , module GHC.Clock
   , module Data.Bits
   , module Control.Applicative
   , module Control.Monad
   , module Data.List
+  , module Data.Bool
   ) where
 
 import Graphics.Gloss.Interface.Pure.Game hiding (shift, rotate)
-import GHC.Clock
 import Data.Bits
 import Control.Applicative
 import Control.Monad(guard, mzero)
 import Data.List(partition)
+import Data.Bool
--- hackage/vaders/vaders.hs
+++ hackage/vaders/vaders.hs
@@ -1,17 +1,20 @@
 #!/usr/bin/env -S stack script --resolver lts-20 --package gloss
-import Imports;tr=translate;o=True;d f=foldMap(\(x,y)->tr x y f);main=(\s->play
- FullScreen white 60((0,(s.&.255)+1,[],0),0,20,mzero)r i st)=<<getMonotonicTimeNSec
-y0= -28;r((_,_,m,z),x,_,b)=text(show z)<>(scale 18 18$tr x y0 sh<>d q b<>d v m)
-sh=circle 1;v=line[(-1,2),(0,0),(1,2)];q=line[(0,0),(0,1)];i(EventKey(Char k)
- Down _ _)(s,x,d,b)|k=='a'=(s,x,-20,b)|k=='d'=(s,x,20,b)|k=='.'=(s,x,d,b<|>Just
- (x,y0));i _ s=s;n(a,b)(x,y)=all(<1)(map abs[a-x,b-y]);st t((w,s,m,z),a,d,b)=((v
- ,p,l,q),a+t*e,e,u)where{p=(s`div`2)`xor`if odd s then 180 else 0;e|a>30= -20|a<
- -30=20|o=d;(c,nc)|Just b<-b=partition(n b)m|o=([], m);m1=map(\(x,y)->(x,y-t*10))
- nc;(v,l)|w>0=(w-t,m1)|o=(0.35,(fromIntegral(s`mod`60)-30,30):m1);u=b>>= \(x,y)
- ->(x,y+t*20)<$guard(null c&&(y<30));q|any(n(a,y0))nc=0|o=z+length c}
+import Imports;tr=translate;o=True;d f=foldMap(\(x,y)->tr x y f);main=play
+ FullScreen white 60((0,1,[],0),0,20,mzero)r i st;y0= -28;r((_,_,m,z),x,_,b)=
+ text(show z)<>(scale 18 18$tr x y0 sh<>d q b<>d v m);sh=circle 1;v=line[(-1,2),
+ (0,0),(1,2)];q=line[(0,0),(0,1)];i(EventKey(Char k)Down _ _)(s,x,d,b)|k=='a'=(s,
+ x,-20,b)|k=='d'=(s,x,20,b)|k=='.'=(s,x,d,b<|>Just(x,y0));i _ s=s;n(a,b)(x,y)=
+ all(<1)(map abs[a-x,b-y]);st t((w,s,m,z),a,d,b)=((v,p,l,q),a+t*e,e,u)where{p=(s
+ `div`2)`xor`bool 0 180 (odd s);e|a>30= -20|a< -30=20|o=d;(c,nc)|Just b<-b=
+ partition(n b)m|o=([], m);m1=map(\(x,y)->(x,y-t*10))nc;(v,l)|w>0=(w-t,m1)|o=(
+ 0.35,(fromInteger(s`mod`60)-30,30):m1);u=b>>= \(x,y)->(x,y+t*20)<$guard(null c
+ &&(y<30));q|any(n(a,y0))nc=0|o=z+length c}
 
 -- ^10 ------------------------------------------------------------------ 80> --
 {- hackage-10-80/vaders (gergoerdi)
 
 
+all(<1)(map abs[a-x,b-y])
+abs(a-x)<1&&abs(b-y)<1
+(a-x)^2<1&&(b-y)^2<1
 -}
