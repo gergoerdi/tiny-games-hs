afd57c3235f7269473eca42753ef842ec14f9d9e
Gergo ERDI <gergo@erdi.hu>
2023-02-13 19:07:51 +0800

    shrink

--- hackage/vaders/vaders.hs
+++ hackage/vaders/vaders.hs
@@ -1,17 +1,11 @@
 #!/usr/bin/env -S stack script --resolver lts-20 --package gloss
-import Imports
-
-main=do{getMonotonicTimeNSec>>= \s->play(InWindow "Vaders" (600,600) (0,0))
-white 60(0,(s`mod`0x10000)+1,0,20,[],Nothing,0)render i step};y0= -28;
-render (_,_,shipX,_,ms,b,z) = (translate (-300) (-250) (text(show z))<>)$scale
- 10 10 $translate shipX y0 sh <>foldMap($q)(fmap (uncurry translate)b)<>foldMap
- ($v)(map (uncurry translate) ms)
-
-sh=polygon[(-1,0),(0,2),(1,0)];v=line[(-1,2),(0,0),(1,2)];q=line[(0,0),(0,1)]
-
-i(EventKey(SpecialKey k)Down _ _)(w,s,x,d,m,b,z)|k==KeyLeft=(w,s,x,-20,m,b,z)|k
- ==KeyRight=(w,s,x,20,m,b,z)|k`elem`[KeyCtrlL,KeyCtrlR]=(w,s,x,d,m,b<|>Just(x,y0)
- ,z);i _ s = s;n(a,b)(x,y)=all(<1)(map abs[a-x,b-y])
+import Imports;tr=translate;d f=foldMap(\(x,y)->tr x y f);main=do{
+getMonotonicTimeNSec>>= \s->play(InWindow "Vaders"(600,600)(0,0))white 60(0,
+(s`mod`0x100)+1,0,20,[],Nothing,0)r i step};y0= -28;r(_,_,x,_,m,b,z)=text(show z
+ )<>(scale 10 10 $tr x y0 sh<>d q b<>d v m);sh=polygon[(-1,0),(0,2),(1,0)];v=
+ line[(-1,2),(0,0),(1,2)];q=line[(0,0),(0,1)];i(EventKey(SpecialKey k)Down _ _)
+ (w,s,x,d,m,b,z)|k==KeyLeft=(w,s,x,-20,m,b,z)|k==KeyRight=(w,s,x,20,m,b,z)|k==
+ KeyCtrlL=(w,s,x,d,m,b<|>Just(x,y0),z);i _ s=s;n(a,b)(x,y)=all(<1)(map abs[a-x,b-y])
 
 
 step t(w,s,a,d,m,b,z)=(w',s',a+t*d',d',m',b',z')where{s'=(s`div`2) `xor` if odd
